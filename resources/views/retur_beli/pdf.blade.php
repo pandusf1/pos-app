a0eff1b3bc54d8a87da2571c6c8a9ab3a5d15c40
pandusf1 <pandusf1@gmail.com>
2026-01-11 19:36:14 +0700

    Update pdf.blade.php

--- resources/views/retur_beli/pdf.blade.php
+++ resources/views/retur_beli/pdf.blade.php
@@ -7,6 +7,7 @@
         table { width: 100%; border-collapse: collapse; }
         th, td { border: 1px solid #000; padding: 6px; }
         th { background: #eee; }
+        tfoot td { font-weight: bold; }
     </style>
 </head>
 <body>
@@ -25,21 +26,36 @@
             <th>Subtotal</th>
         </tr>
     </thead>
+
     <tbody>
+    @php $grandTotal = 0; @endphp
+
     @foreach($retur as $r)
         @foreach($r->detail as $d)
+        @php $grandTotal += $d->subtotal; @endphp
         <tr>
             <td>{{ $r->no_retur_beli }}</td>
             <td>{{ $r->no_beli }}</td>
             <td>{{ $r->tgl_retur }}</td>
             <td>{{ $d->kd_brg }}</td>
             <td align="center">{{ $d->qty_retur }}</td>
-            <td align="right">{{ number_format($d->harga_beli) }}</td>
-            <td align="right">{{ number_format($d->subtotal) }}</td>
+            <td align="right">{{ number_format($d->harga_beli, 0, ',', '.') }}</td>
+            <td align="right">{{ number_format($d->subtotal, 0, ',', '.') }}</td>
         </tr>
         @endforeach
     @endforeach
     </tbody>
+
+    @if($grandTotal > 0)
+    <tfoot>
+        <tr>
+            <td colspan="6" align="right">TOTAL RETUR</td>
+            <td align="right">
+                Rp {{ number_format($grandTotal, 0, ',', '.') }}
+            </td>
+        </tr>
+    </tfoot>
+    @endif
 </table>
 
 </body>
